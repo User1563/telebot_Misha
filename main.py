856cfcffe7da40b7dce073d8d6e6bfad727fae4f
User1563 <151646710+User1563@users.noreply.github.com>
2024-04-21 16:43:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,71 @@
 from telegram.ext import *
-from python22 import Bott
-
-
-class Bot2(Bott):
-    def main(self) -> None:
-        bb = Bott()
-        application = Application.builder().token(self.tokenn).build()
-        # application.add_handler(CommandHandler("start", bb.start))
-        application.add_handler(CommandHandler("help", bb.help))
-        application.add_handler(CommandHandler("gert", bb.gert))
-        application.add_handler(CommandHandler("file", bb.file_text))
-        application.add_handler(CommandHandler("button_2", bb.button_2))
-        application.add_handler(CommandHandler("useful_materials", bb.button_3))
-        application.add_handler(CommandHandler("button_4", bb.button_4))
-        # application.add_handler(CommandHandler("button_4", bb.button_4))
-        # application.add_handler(CommandHandler('keyboard', b.start))
-        conv_handler = ConversationHandler(
-            entry_points=[CommandHandler("start", Bott().start)],
-            states={
-                bb.CONST: [MessageHandler(filters.TEXT, Bott().work_start)]
-            },
-            fallbacks=[CommandHandler("work_start", Bott().work_start)],
-        )
-
-        application.add_handler(conv_handler)
-        application.run_polling()
-
-
-b = Bot2()
-b.main()
+from python22 import *
+from python22 import tokenn, CONST, FILE, MN
 
 
 
+def main() -> None:
+    application = Application.builder().token(tokenn).build()
+    # application.add_handler(CommandHandler("start", bb.start))
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help))
+    application.add_handler(CommandHandler("gert", gert))
+    application.add_handler(CommandHandler("file", file_text))
+    application.add_handler(CommandHandler("useful_materials", button_3))
+    application.add_handler(CommandHandler("audio", button_4))
+    # application.add_handler(CommandHandler("button_4", button_4))
+    application.add_handler(CommandHandler('keyboard', start))
+    application.add_handler(CommandHandler('photo', photo))
+    conv_handler_4 = ConversationHandler(
+        entry_points=[CommandHandler("fio", command_text)],
+        states={
+            MN: [MessageHandler(filters.TEXT, text_file)]
+        },
+        fallbacks=[CommandHandler("text", text_file)],
+    )
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("function", function)],
+        states={
+            CONST: [MessageHandler(filters.TEXT, work_start)],
+        },
+        fallbacks=[CommandHandler("work_start", work_start)],
+    )
+
+    conv_handler_5 = ConversationHandler(
+        entry_points=[CommandHandler("fio", command_text)],
+        states={
+            MN: [MessageHandler(filters.TEXT, text_file)]
+        },
+        fallbacks=[CommandHandler("text", text_file)],
+    )
+
+    conv_handler_6 = ConversationHandler(
+        entry_points=[CommandHandler("write", write)],
+        states={
+            W: [MessageHandler(filters.TEXT, work_write)],
+        },
+        fallbacks=[CommandHandler("work_write", work_write)],
+    )
+
+    conv_handler_7 = ConversationHandler(
+        entry_points=[CommandHandler("start_otz", otzivi)],
+        states={
+            O: [MessageHandler(filters.TEXT, otzivi1)]
+        },
+        fallbacks=[CommandHandler("otz", otzivi)],
+    )
+    application.add_handler(conv_handler_5)
+    application.add_handler(conv_handler)
+    application.add_handler(conv_handler_6)
+    application.add_handler(conv_handler_4)
+    application.add_handler(conv_handler_7)
+
+
+    # application.add_handler(conv_handler)
+    application.run_polling()
+
+
+if __name__ == '__main__':
+    main()
 
